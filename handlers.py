fe592bc692d49916eca9d7be759d14c871b94df1
nxtmuch <borzov.ivan.14@mail.ru>
2025-06-13 01:27:34 +0300

    first

---
+++ handlers.py
@@ -0,0 +1,36 @@
+from aiogram import F, Router
+from aiogram.types import Message
+from aiogram.filters import CommandStart, Command
+from aiogram.fsm.state import State, StatesGroup
+from aiogram.fsm.context import FSMContext
+
+
+from app.generate import ai_generate
+
+CHANNEL_ID = "-1002624384393"
+
+router = Router()
+
+
+class Gen(StatesGroup):
+    wait = State()
+
+
+@router.message(CommandStart())
+async def cmd_start(message: Message):
+    await message.answer('Привет! Я твой личный наставник, готовый помочь тебе лучше разобраться в школьных предметах! Напиши мне предмет и тему, которую ты не понимаешь, и я постараюсь помочь тебе!')
+
+
+@router.message(Command('about'))
+async def cmd_abot(message: Message):
+    await message.answer('Данный чат-бот разработал студент 2-го курса магистратуры ШФ ИвГУ Борзов Иван. Чат-бот работает на модели ИИ "Deepseek V3", и следует заранее прописанному промпту (запросу), цель которого - оказывать поддержку в принятии решений. Все запросы пользователей сохраняются!!!')
+
+
+@router.message()
+async def generating(message: Message, state: FSMContext):
+    log_message = await message.bot.send_message(chat_id=CHANNEL_ID, text=f"User [{message.from_user.first_name}](tg://user?id={message.from_user.id}) said:\n\n{message.text}", parse_mode='Markdown')
+    await state.set_state(Gen.wait)
+    response = await ai_generate(message.text)
+    await log_message.reply(text=f"Bot answered:\n\n{response}", parse_mode='Markdown')
+    await message.answer(response, parse_mode='Markdown')
+    await state.clear()
---
+++ run.py
@@ -0,0 +1,26 @@
+import asyncio
+from aiogram import Bot, Dispatcher
+from config import TG_TOKEN
+
+from app.handlers import router
+
+
+from telegram.ext import ApplicationBuilder, MessageHandler, filters
+
+CHANNEL_ID = "-1002859508302"
+
+
+async def main():
+    bot = Bot(token=TG_TOKEN)
+    dp = Dispatcher()
+    dp.include_router(router)
+    await dp.start_polling(bot)
+
+
+
+if __name__ == '__main__':
+    try:
+        app = ApplicationBuilder().token(TG_TOKEN).build()
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print('Бот выключен')
